e192cb557284bd6c8a8fd362093a6c9a7a4b8646
rebel228 <dinovik@rambler.ru>
2023-04-30 20:56:55 +0300

    feat: add onclick simple text add

--- index.js
+++ index.js
@@ -19,9 +19,37 @@ function createElement(tagName, classes, innerText) {
     return element;
 }
 
+function typeCharacter (event) {
+    event.preventDefault();
+    cursorPosition.focus;
+    cursorPosition.setRangeText(event.target.innerText, cursorPosition.selectionStart, cursorPosition.selectionEnd, "end");
+}
+
 document.body.append(field);
 document.body.append(keyboard);
 
 for (let i = 0; i < keysList.length; i++) {
     keyboard.append(createElement('div', keysList[i].classes, keysList[i].key));
 }
+
+const letters = document.querySelectorAll('.letter');
+const numbers = document.querySelectorAll('.num');
+const cursorPosition = document.querySelector('.field');
+
+letters.forEach(key => {
+    key.addEventListener('mousedown', typeCharacter);
+});
+
+numbers.forEach(key => {
+    key.addEventListener('click', typeCharacter);
+});
+
+
+console.log(letters);
+console.log(numbers);
+
+function  logKey (e) {
+    console.log(e);
+    console.log(cursorPosition);
+}
+document.addEventListener('keydown', logKey);
